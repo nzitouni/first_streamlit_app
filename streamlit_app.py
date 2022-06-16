3bef400808ae2bee48d7197f2c833ce0ccf17bea
nour zitouni <107547067+nzitouni@users.noreply.github.com>
2022-06-16 12:15:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
