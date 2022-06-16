4bfc264b86770808440502db468a967fc4b12bc5
nour zitouni <107547067+nzitouni@users.noreply.github.com>
2022-06-16 10:20:35 +0200

    Update streamlit_app.py
    update the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Fruits Smoothie')
+streamlit.text('Hard Boiled Free-Range Egg')
